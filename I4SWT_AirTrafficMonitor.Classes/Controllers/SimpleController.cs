401fd980cef4e7bd138e3b56102775f167b35be7
madsnk <mads.nielsen.kjaersgaard@gmail.com>
2018-04-05 15:11:02 +0200

    Start on SimpleController for Initial Inegration of TransponderReceiver

---
+++ I4SWT_AirTrafficMonitor.Classes/Controllers/SimpleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using I4SWT_AirTrafficMonitor.Classes.Tracks;
+using TransponderReceiver;
+
+namespace I4SWT_AirTrafficMonitor.Classes.Controllers
+{
+    class SimpleController
+    {
+        private List<ITrack> _tracks;
+
+        private ITransponderReceiver _receiver;
+
+        public SimpleController(ITransponderReceiver receiver)
+        {
+            _receiver = receiver;
+            _receiver.TransponderDataReady += OnNewTrackData;
+        }
+
+        public void OnNewTrackData(object sender, RawTransponderDataEventArgs e)
+        {
+            // for each raw track in eventArgs
+                // create Track object (using TrackFactory)
+                    // if object tag is not already in tracks list
+                        // add to list
+                    // else
+                        // update track in list
+        }
+    }
+}
---
+++ I4SWT_AirTrafficMonitor.InitialIntegration.TestApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4SWT_AirTrafficMonitor.InitialIntegration.TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
