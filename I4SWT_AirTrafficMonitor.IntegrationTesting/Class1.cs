7d6a9c79bc5b0c330093e1b667ac473d2aae72c5
madsnk <mads.nielsen.kjaersgaard@gmail.com>
2018-04-05 09:28:02 +0200

    Add project files.

---
+++ I4SWT_AirTrafficMonitor.IntegrationTesting/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4SWT_AirTrafficMonitor.IntegrationTesting
+{
+    public class Class1
+    {
+        transponder = TransponerReceiver.C
+        powerButton.Pressed += new EventHandler(OnPowerPressed);
+    }
+}
