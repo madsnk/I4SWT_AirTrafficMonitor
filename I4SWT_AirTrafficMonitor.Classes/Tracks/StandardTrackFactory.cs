2f48dd9e21538233c37c6abf515e9825225f2e0b
Rasmus Westland Knudsen <rasmuswestlandknudsen@hotmail.com>
2018-04-05 14:47:48 +0200

    added content to standardTrackFactory

--- I4SWT_AirTrafficMonitor.Classes/Tracks/StandardTrackFactory.cs
+++ I4SWT_AirTrafficMonitor.Classes/Tracks/StandardTrackFactory.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using I4SWT_AirTrafficMonitor.Classes.Tracks;
 
 namespace I4SWT_AirTrafficMonitor.Classes.Tracks
 {
-    class StandardTrackFactory : ITrackFactory
+    public class StandardTrackFactory : ITrackFactory
     {
         public ITrack CreateTrack(string rawTrackData)
         {
-            throw new NotImplementedException();
+            string[] rawdataSplit = rawTrackData.Split(';');
+
+            string tag = rawdataSplit[0];
+            int xcoor = Convert.ToInt32(rawdataSplit[1]);
+            int ycoor = Convert.ToInt32(rawdataSplit[2]);
+            uint altiude = Convert.ToUInt32(rawdataSplit[3]);
+            DateTime timeStamp = DateTime.ParseExact(rawdataSplit[4], "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
+
+            return new Track(tag, xcoor, ycoor, altiude, timeStamp);
         }
     }
 }
