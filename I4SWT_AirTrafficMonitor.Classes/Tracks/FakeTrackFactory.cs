8d004eaa37045c2f4e463559725e0f761591e7a9
Rasmus Westland Knudsen <rasmuswestlandknudsen@hotmail.com>
2018-04-09 12:49:20 +0200

    writing unit test for controller

---
+++ I4SWT_AirTrafficMonitor.Classes/Tracks/FakeTrackFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using I4SWT_AirTrafficMonitor.Classes.Tracks;
+
+namespace I4SWT_AirTrafficMonitor.Classes.Tracks
+{
+    public class FakeTrackFactory : ITrackFactory
+    {
+        public ITrack CreateTrack(string rawTrackData)
+        {
+            return new Track(rawTrackData, 0, 0, 0, new DateTime(0));
+        }
+    }
+}
