9fd4d22b5290bc351c695a9179d382438da16f53
Rasmus Westland Knudsen <rasmuswestlandknudsen@hotmail.com>
2018-04-12 07:23:41 +0200

    log created

---
+++ I4SWT_AirTrafficMonitor.Classes/Log/ILog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4SWT_AirTrafficMonitor.Classes.Log
+{
+    public interface ILog
+    {
+        void Append(string newData);
+    }
+}
---
+++ I4SWT_AirTrafficMonitor.Classes/Log/Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4SWT_AirTrafficMonitor.Classes.Log
+{
+    public class Log : ILog
+    {
+        private string _filePath;
+        private StringBuilder sb = new StringBuilder();
+        //private DirectoryInfo di =  new DirectoryInfo();
+
+        public Log(string filePath)
+        {
+            _filePath = filePath + String.Format("_{0}.csv", DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss"));
+            //_filePath = filePath;
+        }
+
+        public void Append(string newData)
+        {
+            File.AppendAllText(_filePath, newData);
+        }
+    }
+}
+
