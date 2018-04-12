c70f8987bb01ac9c25dcd3ccacf72f6d6f15d6fc
Rasmus Westland Knudsen <rasmuswestlandknudsen@hotmail.com>
2018-04-12 09:43:48 +0200

    log unit test

---
+++ I4SWT_AirTrafficMonitor.UnitTesting/LogUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using I4SWT_AirTrafficMonitor.Classes.Log;
+using System.IO;
+
+namespace I4SWT_AirTrafficMonitor.UnitTesting
+{
+    [TestFixture]
+    class LogUnitTest
+    {
+        private ILog _uut;
+        private string timeString;
+        [SetUp]
+        public void Setup()
+        {
+            timeString = DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss");
+            _uut = new Log("UnitTestLog");
+            
+        }
+
+        [Test]
+        public void Log_newLog_LogExixst()
+        {
+            FileAssert.Exists("UnitTestLog" + "_" + timeString + ".csv");
+        }
+
+        [Test]
+        public void Log_newLog_HeaderIsCorrect()
+        {
+            var s = File.ReadAllLines("UnitTestLog" + "_" + timeString + ".csv");
+            Assert.AreEqual("Time Of Occurrence;First Track Tag;Second Track Tag", s[0]);
+        }
+
+        [Test]
+        public void Append_addData_dataInLog()
+        {
+            _uut.Append("2018-04-12 08.59.39.481;XXX123;YYY456\n");
+
+            var s = File.ReadAllLines("UnitTestLog" + "_" + timeString + ".csv");
+            Assert.AreEqual("2018-04-12 08.59.39.481;XXX123;YYY456", s[1]);
+        }
+        
+
+    }
+}
