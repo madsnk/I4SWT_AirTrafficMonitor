d453854f326f5af4d897711a5534fe7391a91565
Rasmus Westland Knudsen <rasmuswestlandknudsen@hotmail.com>
2018-04-16 15:00:20 +0200

    IT 3 finish

--- I4SWT_AirTrafficMonitor.IntegrationTesting/IT3_ATMControllerSeparationEventAirSpace.cs
+++ I4SWT_AirTrafficMonitor.IntegrationTesting/IT3_ATMControllerSeparationEventAirSpace.cs
@@ -20,22 +20,14 @@ namespace I4SWT_AirTrafficMonitor.IntegrationTesting
         private ATMController _uut_atmController;
         private ILog _uut_log;
         private ITrackFactory _uut_trackFactory;
-        private ITrack _uut_track;
-        private ISeperationEvent _uut_seperationEvent;
         private IAirSpace _uut_airSpace;
+        private List<ITrack> _uut_tracks;
+        private List<ISeperationEvent> _uut_seperationEvents;
 
         private ITransponderReceiver _receiver;
         private IConsoleWrapper _console;
-        private List<ITrack> _tracks;
-        private List<ISeperationEvent> _seperationEvents;
-
-        private string testTag = "XXX123";
-        private DateTime testTime = new DateTime(2017, 10, 10, 10, 10, 10, 0);
-        private int testXPos = 100;
-        private int testYPos = 100;
-        private int testAlt = 3000;
-
-        //private string _timeString;
+        
+        private string _timeString;
 
         [SetUp]
         public void SetUp()
@@ -43,20 +35,18 @@ namespace I4SWT_AirTrafficMonitor.IntegrationTesting
             _console = Substitute.For<IConsoleWrapper>();
             _receiver = Substitute.For<ITransponderReceiver>();
 
+            _uut_tracks = new List<ITrack>();
 
-            _tracks = new List<ITrack>();
-
-            _uut_seperationEvent = new SeperationEvent();
-            _seperationEvents = new List<ISeperationEvent>();
+            _uut_seperationEvents = new List<ISeperationEvent>();
 
             _uut_trackFactory = new StandardTrackFactory();
-            _uut_track = new Track(testTag, testXPos, testYPos, testAlt, testTime);
             _uut_airSpace = new AirSpace(10000, 10000, 90000, 90000, 500, 20000, 300, 5000);
 
             _uut_log = new Log("IntergrationTestLog");
+            _timeString = DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss");
 
-            _uut_atmController = new ATMController(_receiver, _uut_trackFactory, _console, _uut_airSpace, _tracks,
-                _seperationEvents, _uut_log);
+            _uut_atmController = new ATMController(_receiver, _uut_trackFactory, _console, _uut_airSpace, _uut_tracks,
+                _uut_seperationEvents, _uut_log);
         }
 
         [Test]
@@ -73,6 +63,20 @@ namespace I4SWT_AirTrafficMonitor.IntegrationTesting
             _console.Received(1).Report("Time:           2017-12-12 12.12.20.112" + "\r\nInvolved Tags:  XXX123, YYY321\r\nHorizontal Sep: 1\r\nVertical Sep:   1\r\n");
         }
 
+        [Test]
+        public void Log_AddTwoConflictingTracks_dataInLog()
+        {
+            var fakeStrings = new List<string>
+            {
+                "XXX123;10000;10500;15000;20171212121220111",
+                "YYY321;10001;10501;15001;20171212121220112"
+            };
+
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(fakeStrings));
+
+            var s = File.ReadAllLines("IntergrationTestLog" + "_" + _timeString + ".csv");
+            Assert.AreEqual("2017-12-12 12.12.20.112;XXX123;YYY321", s[1]);
+        }
 
     }
     
