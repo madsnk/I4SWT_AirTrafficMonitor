d4bf6af180d5c157505472cd31cceca916d1151f
madsnk <mads.nielsen.kjaersgaard@gmail.com>
2018-04-16 14:02:50 +0200

    Finish IT2 - and remove dummytest from SimpleController IT test

--- I4SWT_AirTrafficMonitor.IntegrationTesting/IT2_ATMController_Track_TrackFactory.cs
+++ I4SWT_AirTrafficMonitor.IntegrationTesting/IT2_ATMController_Track_TrackFactory.cs
@@ -53,9 +53,45 @@ namespace I4SWT_AirTrafficMonitor.IntegrationTesting
             };
 
             _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(testData));
-            Assert.That(_tracks[0].Xcoor, Is.EqualTo(10000));
-            Assert.That(_tracks[0].Tag, Is.EqualTo("XXX123"));
-            Assert.That(_tracks[0].TimeStamp, Is.EqualTo(new DateTime(2017, 11, 22, 11, 22, 33, 100)));
+            Assert.That(_drivenUut_controller.GetTracks()[0].Xcoor, Is.EqualTo(10000));
+            Assert.That(_drivenUut_controller.GetTracks()[0].Tag, Is.EqualTo("XXX123"));
+            Assert.That(_drivenUut_controller.GetTracks()[0].TimeStamp, Is.EqualTo(new DateTime(2017, 11, 22, 11, 22, 33, 100)));
+        }
+
+        [Test]
+        public void UpdateTrack_updateTrackObjWithNewRawTrackData_VelocityUpdatedCorrectly()
+        {
+            var testData1 = new List<string>
+            {
+                "XXX123;10000;10000;15000;20171122112233100"
+            };
+            var testData2 = new List<string>
+            {
+                "XXX123;10000;10200;15000;20171122112234100" // Add one second and 200m in y coordinate
+            };
+
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(testData1));
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(testData2));
+
+            Assert.That(_drivenUut_controller.GetTracks()[0].Velocity, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void UpdateTrack_updateTrackObjWithNewRawTrackData_CourseUpdatedCorrectly()
+        {
+            var testData1 = new List<string>
+            {
+                "XXX123;10000;10000;15000;20171122112233100"
+            };
+            var testData2 = new List<string>
+            {
+                "XXX123;12000;12000;15000;20171122112213100" // Add 10 second and 2000m in y and x coordinate(45deg)
+            };
+
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(testData1));
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(testData2));
+
+            Assert.That(_drivenUut_controller.GetTracks()[0].Course, Is.EqualTo(45));
         }
     }
 }
